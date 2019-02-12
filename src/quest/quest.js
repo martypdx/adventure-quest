1df23d714a91fdbe581c23c796dffc70e96bf038
martypdx <marty@martypdx.com>
2019-02-12 06:35:08 -0800

    show selected quest

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -1,15 +1,52 @@
 import loadUser from '../load-user.js';
 import quests from '../quests.js';
-import findById from '../find-by-id/js';
+import findById from '../find-by-id.js';
 
 loadUser();
 
 const searchParams = new URLSearchParams(window.location.search);
 const questId = searchParams.get('id');
-const search = findById(quests, questId);
+const quest = findById(quests, questId);
 
-if(!search) {
+if(!quest) {
     window.location = 'map.html';
 }
 
+const title = document.getElementById('title');
+const image = document.getElementById('image');
+const audio = document.getElementById('audio');
+const description = document.getElementById('description');
+const choiceForm = document.getElementById('choice-form');
+const choices = document.getElementById('choices');
+const result = document.getElementById('result');
+const back = document.getElementById('back');
 
+title.textContent = quest.title;
+image.src = 'assets/quests/' + quest.image;
+// audio.src = 'assets/quests/' + quest.audio;
+description.textContent = quest.description;
+
+for(let index = 0; index < quest.choices.length; index++) {
+    const choice = quest.choices[index];
+
+    const label = document.createElement('label');
+    label.classList.add('choice');
+
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'choice';
+    radio.required = true;
+    radio.value = choice.id;
+    label.appendChild(radio);
+
+    const description = document.createElement('span');
+    description.textContent = choice.description;
+    label.appendChild(description);
+
+    choices.appendChild(label);
+}
+
+choiceForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+});
