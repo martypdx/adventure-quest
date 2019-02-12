259a4b1495fed831f91aa4793589f1cef0b29bee
martypdx <marty@martypdx.com>
2019-02-12 07:00:26 -0800

    add stats to user profile

--- src/load-user.js
+++ src/load-user.js
@@ -11,9 +11,13 @@ function loadUser() {
 
     const name = document.getElementById('name');
     const avatar = document.getElementById('avatar');
+    const hp = document.getElementById('hp');
+    const gold = document.getElementById('gold');
 
     name.textContent = user.name;
     avatar.src = 'assets/avatars/' + user.race + '.png';
+    hp.textContent = user.hp;
+    gold.textContent = user.gold;
 
     return user;
 }
