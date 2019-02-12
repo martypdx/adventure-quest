b67521fa88c2380ec8ba44ba48048f999cdf5c73
martypdx <marty@martypdx.com>
2019-02-12 07:31:05 -0800

    Add you are dead ending

--- src/end/end.js
+++ src/end/end.js
@@ -1,3 +1,14 @@
 import loadUser from '../load-user.js';
 
-loadUser();
+const user = loadUser();
+const storyDisplay = document.getElementById('storyDisplay');
+
+let story = 'Well, ' + user.name + ' the ' + user.race + ', ';
+
+if(user.hp <= 0) {
+    story += 'you are dead.';
+}
+
+
+
+storyDisplay.textContent = story;
